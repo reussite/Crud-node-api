bc452c6c8deb4e40feaf0aa4a17a4ca3d70b13f8
pascaldalmeida9 <pascaldalmeida9@gmail.com>
2023-09-24 20:41:29 +0100

    getAllstudent and deleted student Controllers created

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -3,8 +3,15 @@ const studentModel = require("../models/Student");
 module.exports = {
   createStudent: async (req, res) => {
     try {
-      const { student_name, firstName, email, birthday, nationnality, contact, phone } =
-        req.body;
+      const {
+        student_name,
+        firstName,
+        email,
+        birthday,
+        nationnality,
+        contact,
+        phone,
+      } = req.body;
 
       const newStudent = await studentModel.studentRegister({
         student_name: student_name,
@@ -17,12 +24,14 @@ module.exports = {
       });
       res.status(200).json({
         message: "Student profil insertion successfuly.",
-        newStudent,
+        status: true,
+        student: newStudent,
       });
     } catch (error) {
-      res.status(400).json({
+      /* res.status(400).json({
         message: "Student profil insertion failed.",
-      });
+      }); */
+      console.log(error);
     }
   },
   updateStudent: async (req, res) => {
@@ -68,10 +77,33 @@ module.exports = {
   delectStudent: async (req, res) => {
     try {
       const studentId = +req.params.studentId;
+      console.log(studentId);
       const deleteStudentData = await studentModel.studentDelete(studentId);
-      res.status(200).json({ message: "Student deleted", deleteStudentData });
+      res.status(200).json({
+        message: "Student deleted",
+        status: true,
+        information: deleteStudentData,
+      });
     } catch (error) {
-      res.status(400).json({ message: "Error deleting student", error });
+      /* res.status(400).json({
+        message: "Error deleting student",
+        error,
+      }); */
+      console.log(error);
+    }
+  },
+
+  getAllStudentListe: async (req, res) => {
+    try {
+      const allStudent = await studentModel.studentList();
+
+      res.status(200).json({
+        message: "Student List",
+        status: "success",
+        students: allStudent,
+      });
+    } catch (error) {
+      console.log(error);
     }
   },
 };
--- routes/student.js
+++ routes/student.js
@@ -4,5 +4,8 @@ const studentController = require("../controllers/studentController");
 
 router.post("/register", studentController.createStudent);
 router.put("/update/:id", studentController.updateStudent);
+router.delete("/delete/:studentId", studentController.delectStudent);
+router.get("/find/:studentId", studentController.findStudent);
+router.get("/list", studentController.getAllStudentListe);
 
 module.exports = router;
